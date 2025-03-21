770975945ffd28f124dae3992ceef8e4a5a26f63
Nicholas Schell <nicholas.schell@dolby.com>
2025-03-20 21:41:33 -0700

    drop tests requiring System.Collections.Specialized

--- test/Twilio.Test/Security/RequestValidatorTest.cs
+++ test/Twilio.Test/Security/RequestValidatorTest.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
+//using System.Collections.Specialized;
 using NUnit.Framework;
 using Twilio.Security;
 
@@ -12,19 +12,20 @@ namespace Twilio.Tests.Security
         private const string Url = "https://mycompany.com/myapp.php?foo=1&bar=2";
         private const string Body = "{\"property\": \"value\", \"boolean\": true}";
         private const string BodyHash = "0a1ff7634d9ab3b95db5c9a2dfe9416e41502b283a80c7cf19632632f96e6620";
-        private readonly NameValueCollection _parameters = new NameValueCollection();
+        //private readonly NameValueCollection _parameters = new NameValueCollection();
         private readonly RequestValidator _validator = new RequestValidator("12345");
 
         public RequestValidatorTest()
         {
             // Intentionally out of alphabetical order
-            _parameters.Add("CallSid", "CA1234567890ABCDE");
+            /*_parameters.Add("CallSid", "CA1234567890ABCDE");
             _parameters.Add("From", "+14158675309");
             _parameters.Add("Digits", "1234");
             _parameters.Add("To", "+18005551212");
-            _parameters.Add("Caller", "+14158675309");
+            _parameters.Add("Caller", "+14158675309");*/
         }
 
+/*
         [Test]
         public void TestValidateDictionary()
         {
@@ -117,6 +118,7 @@ namespace Twilio.Tests.Security
             const string signature = "RSOYDt4T1cUTdK1PDd93/VVr8B8=";
             Assert.IsTrue(_validator.Validate(Url, _parameters, signature), "Request does not match provided signature");
         }
+*/
 
         [Test]
         public void TestValidateBody()
@@ -144,6 +146,7 @@ namespace Twilio.Tests.Security
             Assert.IsFalse(_validator.Validate(Url, Body, "RSOYDt4T1cUTdK1PDd93/VVr8B8="), "Request signature or body validation failed");
         }
 
+/*
         [Test]
         public void TestValidateRemovesPortHttps()
         {
@@ -184,5 +187,6 @@ namespace Twilio.Tests.Security
             const string url = "http://mycompany.com:1234/myapp.php?foo=1&bar=2";
             Assert.IsTrue(_validator.Validate(url, _parameters, "Zmvh+3yNM1Phv2jhDCwEM3q5ebU="), "Request does not match provided signature");
         }
+*/
     }
 }
